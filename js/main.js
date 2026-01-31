2e27b8b2b23857617630c5d4822cc6c1736f6423
David AHn <davidahn@Mac.fios-router.home>
2026-01-31 02:37:31 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,59 @@
 
 
-const inTitle=document.querySelector("#input-title")
-const inPrice=document.querySelector("#input-price")
-const inDescription=document.querySelector("#input-description")
-const btnCancel=document.querySelector("#button-cancel")
+// const inTitle=document.querySelector("#input-title")
+// const inPrice=document.querySelector("#input-price")
+// const inDescription=document.querySelector("#input-description")
+// const btnCancel=document.querySelector("#button-cancel")
 
 
-const onClickCancel=(evt)=>{
-    ///Don't navigate to a different page///
-    evt.preventDefault()
-    inTitle.value=""
-    inPrice.value=0;
-    inDescription.value=""
+// const onClickCancel=(evt)=>{
+//     ///Don't navigate to a different page///
+//     evt.preventDefault()
+//     inTitle.value=""
+//     inPrice.value=0;
+//     inDescription.value=""
+// }
+
+// btnCancel.addEventListener("click",onClickCancel)
+
+import Listings from "../Listings";
+
+// function mainModule(name="Main Module"){
+
+//     const me={};
+
+//     console.log(`${name} loaded`);
+    
+//     let count=0;
+
+//     function incre(){
+//         count +=1;
+//         return count;
+//     }
+//     me.incre=incre;
+//     me.name=name;
+//     return me;
+// }
+
+// const main= mainModule();
+// const secondary= mainModule("Secondary Module");
+
+
+// console.log("main--->",main);
+// console.log("main incre--->",main.incre());
+// console.log("main incre--->",main.incre());
+
+// console.log("secondary incre--->",secondary.incre());
+// console.log("main incre--->",main.incre());
+
+
+
+function mainModule(){
+
+    const me={};
+
+    me.listings=Listings();
+    return me;
 }
 
-btnCancel.addEventListener("click",onClickCancel)
+mainModule()
